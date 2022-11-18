099aed60e9bfb2335c05e21dc18d8593935983b1
Alejandro Salas <asalas.wd@outlook.com>
2022-11-18 11:30:49 -0500

    Create SurveyPanel class

--- src/com/java21days/SurveyWizard.java
+++ src/com/java21days/SurveyWizard.java
@@ -1,4 +1,18 @@
 package com.java21days;
 
-public class SurveyWizard {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SurveyWizard extends JFrame implements ActionListener {
+    int currentCard = 0;
+    CardLayout cards = new CardLayout();
+    SurveyPanel[] ask = new SurveyPanel[];
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
 }
